4a097ee34c83e1b795a74403fcbb12545e95f609
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-03-01 21:22:24 -0600

    code style, and make <form> building coordinate with class function

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,13 +1,20 @@
 {{--
-    Renders a basic form for managing the given resources
+	Renders a basic form for managing the given resources
 --}}
-<form role="form" action="{{ $action }}" method="{{ $method }}"@foreach($attributes as $attr => $val) {{ $attr . '=' . $val  }}@endforeach>
-    <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+<form role="form" method="{{ $method }}"
+	@if($action)
+		action="{{ $action }}"
+	@endif
+	@foreach($attributes as $attr => $val)
+		{{ $attr }}="{{ $val }}"
+	@endforeach
+>
+<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-    {{--Render the resource inputs--}}
-    @foreach($resources as $resource)
-        {{ $resource->renderInput() }}
-    @endforeach
+{{--Render the resource inputs--}}
+@foreach($resources as $resource)
+	{{ $resource->renderInput() }}
+@endforeach
 
-    <button type="submit" class="btn btn-primary">Submit</button>
+	<button type="submit" class="btn btn-primary">Submit</button>
 </form>
