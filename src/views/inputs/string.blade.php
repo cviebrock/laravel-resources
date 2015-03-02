cbaf9beb9f6df574a0824d586c083ec22c8ae599
bmartel <brandonmartel@gmail.com>
2015-03-01 20:24:50 -0600

    add some laravel style form input validations

--- src/views/inputs/string.blade.php
+++ src/views/inputs/string.blade.php
@@ -1,4 +1,5 @@
-<div class="form-group">
-    <label for="{{ $name }}">{{ $label }}</label>
-    <input class="form-control" id="{{ $id }}" name="{{ $name }}" type="text" value="{{ $value }}"/>
+<div class="form-group{{ ($errors->has($id)) ? ' has-error': '' }}">
+    <label class="control-label" for="{{ $name }}">{{ $label }}</label>
+    <input id="{{ $id }}" class="form-control" name="{{ $name }}" type="text" value="{{ $value }}"/>
+    {{ $errors->first($id, '<span class="help-block">:message</span>') }}
 </div>
