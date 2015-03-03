1e325d61da491cf442ef2d15d224da803ca2addc
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-03-03 17:13:39 -0600

    tweak for empty inputs

--- src/views/inputs/checkboxes.blade.php
+++ src/views/inputs/checkboxes.blade.php
@@ -2,11 +2,13 @@
 
 @section('input.content')
 	<label>{{ $label }}</label>
+	{{-- hidden element to force the input to be there, even if all the checkboxes are empty --}}
+	<input type="hidden" name="{{ $name }}">
 	@foreach( $choices as $_key => $_choice )
 		<div class="checkbox">
 			<label>
 				<input type="checkbox" value="{{ $_key }}" id="{{ $id }}" name="{{ $name }}[]"
-					@if(in_array($_key, $value))
+					@if(is_array($value) && in_array($_key, $value))
 						checked="checked"
 					@endif
 				>
