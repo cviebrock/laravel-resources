3c32d7ec1b9101d80cb4edcbd57d14233f053f47
bmartel <brandonmartel@gmail.com>
2015-06-01 18:57:38 -0500

    updated for laravel5

---
+++ config/resources.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Here is where you define all the resources your application needs.
+ * You can make it a nested array, or use dot-notation.
+ *
+ * The values of the array represent the resource descriptor classes
+ * that are used
+ */
+
+return [
+
+
+    /**
+     * The prefix applied to table names used by the package.
+     *
+     * By default, the package uses tables called:
+     *
+     *  - resources
+     *  - resource_translations
+     *
+     * If those conflict with tables already in your application,
+     * then just supply a prefix here to avoid conflict.
+     *
+     * Note: This is in addition to any database-wide prefix defined
+     * in /app/config/database.php
+     */
+    'tablePrefix' => '',
+
+    /**
+     * The cache prefix used to store the settings.
+     */
+    'cachePrefix' => 'resources',
+
+    'resources' => [
+
+        //  'home.title' => 'My App'
+
+    ]
+
+];
